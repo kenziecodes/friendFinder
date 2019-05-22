2ce22ad5b2d2a2142627d1f5b56b97ac543e8563
Mackenzie <kenziecodes@gmail.com>
2019-05-22 14:09:26 -0700

    init commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get("/", function(req, res){
+    res.send("Hello World");
+});
+
+app.listen(PORT, () => console.log(`App listening on prot ${PORT}`));
